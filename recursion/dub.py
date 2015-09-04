0c658a51c37e9a1ec30cf3a48b4b97a553ea14a8
Kapil Sharma <qakapil@gmail.com>
2015-09-04 17:12:38 +0200

    sum combi

--- recursion/dub.py
+++ recursion/dub.py
@@ -12,4 +12,17 @@ array that add up to 15, namely:
 """
 
 def combi_nums_sum(number, target, partial=[]):
-    if
+    s = sum(partial)
+
+    # check if the partial sum is equals to target
+    if s == target:
+        print "sum(%s)=%s" % (partial, target)
+    if s >= target:
+        return  # if we reach the number why bother to continue
+
+    for i in range(len(numbers)):
+        n = numbers[i]
+        remaining = numbers[i+1:]
+        subset_sum(remaining, target, partial + [n])
+
+subset_sum([1,2,3,4],6)
